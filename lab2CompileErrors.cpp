bda0a7c6982c01923866e41795d65d1833960e35
SaeedMNU <sn1153@live.mdx.ac.uk>
2023-10-27 10:38:27 +0100

    creation

---
+++ lab2CompileErrors.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  int x;
+  int y;
+  std::cout << "Please enter two numbers:";
+  std::cin >> x >> y;
+  std::cout << "The sum of " << x << "and" << y << "is: " << x + y << std::endl;
+}
---
+++ stats.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  double x;
+  double y;
+  std::cout << "Enter two numbers: ";
+  std::cin >> x >> y;
+  std::cout << "The sum of " << x << "and" << y << "is: " << x + y << std::endl;
+}
